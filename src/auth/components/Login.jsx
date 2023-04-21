43e2a22864fa48562be0bcffb7ada89b2a3e20d8
DREWiex <aleon88@gmail.com>
2023-04-21 08:53:00 +0200

    Creo form en componente Login

--- src/auth/components/Login.jsx
+++ src/auth/components/Login.jsx
@@ -1,14 +1,23 @@
 
 export const Login = () => {
 
+    
 
     return (
 
         <>
 
-            <section>
-                Login section
-            </section>
+            <form>
+
+                <label htmlFor="username">Username</label>
+                <input type="text" />
+
+                <label htmlFor="password"> Password </label>
+                <input type="password" />
+
+                <input type="submit" value="Send" />
+
+            </form>
 
         </>
 
